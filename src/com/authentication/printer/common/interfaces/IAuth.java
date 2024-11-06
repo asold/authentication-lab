a47591e5134e9c26b4ba1723d123bd4b94305df3
asold <asoldan1459@gmail.com>
2024-11-06 14:00:23 +0100

    Changed auth methods

--- src/com/authentication/printer/common/interfaces/IAuth.java
+++ src/com/authentication/printer/common/interfaces/IAuth.java
@@ -3,7 +3,10 @@ package interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import com.authentication.printer.server.helpers.Token;
+
 public interface IAuth extends Remote {
-    public boolean Authenticate(String username, String password) throws RemoteException;
-    public boolean AuthorizeJWT(String username) throws RemoteException; 
+    public Token Authenticate(String username, String password) throws RemoteException;
+    
+    public boolean AuthorizeJWT(Token token) throws RemoteException; 
 }
